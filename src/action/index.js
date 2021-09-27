b3e452ab3e99b1cb373fc85c9f6963454ffb9ea2
oluwadare <wintan1418@gmail.com>
2021-09-27 11:39:58 +0100

    Make json async fetch method for fetching readings

--- src/action/index.js
+++ src/action/index.js
@@ -8,4 +8,15 @@ const fetchReadings =() => (dispatch) => {
   const abortCont = new AbortController();
   const url = 'heroku';
   const token = sessionStorage.getItem('token')
-}
+  fetch(url, {
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => dispatch({
+      type: FETCH_READINGS,
+      payload: data,
+
+    }));
+};
