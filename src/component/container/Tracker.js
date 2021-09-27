e5c04fb6435558056d4ba747a341b74f656fd7e7
oluwadare <wintan1418@gmail.com>
2021-09-27 13:10:43 +0100

    Finish Tracker functions

--- src/component/container/Tracker.js
+++ src/component/container/Tracker.js
@@ -10,5 +10,26 @@ const Track = ({fetchReadings, fetchDetailReadings, readings}) => {
     fetchReadings();
 
   }, [fetchReadings]);
-  
+  const clickOnDetailReadings = (e) => {
+    fetchDetailReadings(e);
+  };
+
+  if (sessionStorage.getItem('token') === 'undefined' || sessionStorage.getItem('token') === null) {
+   return <Redirect to="/log_in" />;
+}
+return readings.readings !== undefined
+  ? (<TraceMap readings={readings.readings} clickOnDetailReadings={clickOnDetailReadings} />)
+  : <h3>Loading...</h3>;
+};
+
+const mapStateToProps = (state) => ({
+  readings: state.readings,
+})
+
+export default connect(mapStateToProps, { fetchReadings, fetchDetailReadings })(Track);
+
+Track.propTypes = {
+  readingss: PropTypes.instanceOf(Object).isRequired,
+  fetchreadingss: PropTypes.func.isRequired,
+  fetchDetailreadings: PropTypes.func.isRequired,
 };
