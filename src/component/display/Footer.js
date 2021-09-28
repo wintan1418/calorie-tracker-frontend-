7979a792c09b1ed2befd739d5a10a80826eea193
oluwadare <wintan1418@gmail.com>
2021-09-28 09:03:59 +0100

    Build footer and set paths to links

---
+++ src/component/display/Footer.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => (
+  <footer>
+    <ul className="f-con">
+      <li>
+        <Link to={{
+          pathname: '/measurement',
+        }}
+        >
+          <span className="f-icons">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" />
+            </svg>
+          </span>
+          <div className="f-text">Add measure</div>
+        </Link>
+      </li>
+      <li>
+        <Link to={{
+          pathname: '/track',
+        }}
+        >
+          <span className="f-icons">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clipRule="evenodd" />
+            </svg>
+          </span>
