4798953ee69a9deb72fa525f138dcf7f2075a644
oluwadare <wintan1418@gmail.com>
2021-09-28 09:12:40 +0100

    Add method to handle user signup

--- src/component/display/Register.js
+++ src/component/display/Register.js
@@ -17,3 +17,18 @@ const SignUp = () => {
         history.push('/measurement');
       });
   }
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const userInput = {
+      name: e.target[0].value,
+      email: e.target[1].value,
+      password: e.target[2].value,
+      password_confirmation: e.target[3].value,
+      level_initial: e.target[4].value,
+      level_plan: e.target[5].value,
+      due_date: e.target[6].value,
+    };
+
+    const urlSignUp = 'heroku/signup';
+    postData(urlSignUp, userInput);
+  };
