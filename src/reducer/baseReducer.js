0a1668d7704e28bf38d22fdb2b08a73c53f63f95
oluwadare <wintan1418@gmail.com>
2021-09-30 10:49:18 +0100

    combine reducer methods in base reducer

---
+++ src/reducer/baseReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import readingsReducer from "./readings";
+import currentUserReducer from "./currentuser";
+import detailReadingReducer from "./detailReading";
+
+const baseReducer = combineReducers({
+  readings: readingsReducer,
+  detailReading: detailReadingReducer,
+  currentUser: currentUserReducer
+
+  
+});
+
+export default baseReducer;
