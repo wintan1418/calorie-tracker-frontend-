124db8172bdb88216e14f47c8719fa0e0d271ae1
oluwadare <wintan1418@gmail.com>
2021-09-30 10:41:47 +0100

    Add reducer logic for detail readings

---
+++ src/reducer/detailReading.js
@@ -0,0 +1,15 @@
+import { FETCH_DETAIL_READINGS } from "../action/categories";
+
+const detailReadingReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_DETAIL_READINGS:
+      return {
+        ...state,
+        detailReading: action.payload,
+
+      };
+       default:
+         return state;
+  }
+};
+export default detailReadingReducer
