8291290db5fa0be23dceabacb8e052f82586119b
oluwadare <wintan1418@gmail.com>
2021-10-07 08:15:27 +0100

    Add test for navbar

--- src/myTest/Navbar.test.js
+++ src/myTest/Navbar.test.js
@@ -1,1 +1,9 @@
- 
+ import React from 'react';
+ import renderer from 'react-test-renderer';
+ import Navbar from '../component/display';
+
+ it('renders accurately', () => {
+   const tree = renderer.create(<Navbar title="more"/>).toJson();
+   expect(tree).toMatchSnapshot();
+
+ });
